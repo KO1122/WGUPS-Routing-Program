45cf70e2044c11d12c017cf3d0e21144655950c8
Kyle Radcliffe Ong <kyleradcliffeong@Kyles-MacBook-Air.local>
2024-04-27 16:40:25 +0800

    Created Truck class

--- truck.py
+++ truck.py
@@ -1,3 +1,9 @@
 class Truck:
-    def __init__(self):
+    def __init__(self, current_location, speed, miles, packages, depart_time):
+        self.current_location = current_location 
+        self.speed = speed
+        self.miles = miles 
+        self.packages = packages 
+        self.depart_time = depart_time
+        self.time = depart_time
         
